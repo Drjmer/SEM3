76e9676613a4c98c5fba23284b24d1516a74098a
drj_47 <dhruvrajmerchant.123@gmail.com>
2024-08-11 12:18:24 +0530

    Windows

--- CS213_DSA/lab02/lab02/circular-vs-linear/circularQueue.cpp
+++ CS213_DSA/lab02/lab02/circular-vs-linear/circularQueue.cpp
@@ -4,23 +4,50 @@
 using namespace std;
 
 template <typename T> bool DynamicQueue<T> :: isEmpty() {
-  return true; // dummy return
+
+  return head==tail; // dummy return
 }
 
 template <typename T> bool DynamicQueue<T> :: isFull() {
-  return true; // dummy return
+  return size()==N-1; // dummy return
 }
 
 template <typename T> void DynamicQueue<T> :: grow() {
+  T inital_size=size();
+  N=nextSize();
+  T* tmp=new T[N];
+  for(int i=head;i<inital_size+1+head;i++)
+  {
+    tmp[i]=A[(i+head)%inital_size];
+  }
+  delete [] A;
+  head=0;
+  tail=inital_size+1;
+  A=tmp;
 }
 
 template <typename T> unsigned int DynamicQueue<T> :: size() {
-  return 0; // dummy return
+  return (tail-head+N)%N; // dummy return
 }
 
 template <typename T> void DynamicQueue<T> :: QInsert(T x) {
+  if(isFull()){
+    grow();
+    A[tail]=x;
+    tail=(tail+1)%N;
+  }
+  else{
+    A[tail]=x;
+    tail=(tail+1)%N;
+  }
 }
 
 template <typename T> bool DynamicQueue<T> :: QDelete(T* x) {
+  if(isEmpty()){
+    return false;
+  }
+  x=A+head;
+  head=(head+1+N)%N;
+  if(head==tail) head=tail=0;
   return true; // dummy return
 }
--- CS213_DSA/lab02/lab02/circular-vs-linear/linearQueue.cpp
+++ CS213_DSA/lab02/lab02/circular-vs-linear/linearQueue.cpp
@@ -4,23 +4,62 @@
 using namespace std;
 
 template <typename T> bool DynamicQueue<T> :: isEmpty() {
-  return true; // dummy return
+  return head==tail; // dummy return
 }
 
 template <typename T> bool DynamicQueue<T> :: isFull() {
-  return true; // dummy return
+  return tail-head==N-1; // dummy return
 }
 
 template <typename T> void DynamicQueue<T> :: grow() {
+  N=nextSize();
+  T* tmp=new T[N];
+  for(int i=0;i<tail;i++)
+  {
+    tmp[i]=A[i];
+  }
+  delete [] A;
+  A=tmp;
 }
 
 template <typename T> unsigned int DynamicQueue<T> :: size() {
-  return 0; // dummy return
+  return tail-head; // dummy return
 }
 
 template <typename T> void DynamicQueue<T> :: QInsert(T x) {
+  if(isFull())
+  {
+    grow();
+    A[tail]=x;
+    tail++;
+  }
+  else{
+    if(head!=0 && tail=N-1){
+      for(int i=0;i<size()+1;i++){
+        A[i]=A[i+head];
+      }
+      tail=size()+1;
+      A[tail]=x;
+      tail++;
+    }
+    else{
+      A[tail]=x;
+      tail++;
+    }
+  }
 }
 
 template <typename T> bool DynamicQueue<T> :: QDelete(T* x) {
-  return true; // dummy return
+  if(isEmpty())
+  {
+    return false;
+    
+  }
+  else{
+    x=A+head;
+    head++;
+    if(head==tail) head=tail=0;
+    return true;
+  }
+
 }
